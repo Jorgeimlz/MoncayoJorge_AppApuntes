a94a3040aab7ea515bd66d9bbfa2129db736f021
Jorge Isaac Moncayo Loaiza <jorge.moncayo.loaiza@udla.edu.ec>
2023-11-29 10:09:03 -0500

    Trabajo en clase terminado, tutorial finalizado.

---
+++ Models/AllNotes.cs
@@ -0,0 +1,41 @@
+﻿using MoncayoJorge_AppApuntes.Views;
+using System.Collections.ObjectModel;
+
+namespace MoncayoJorge_AppApuntes.Models;
+
+internal class AllNotes
+{
+    public ObservableCollection<NoteJorge> Notes { get; set; } = new ObservableCollection<NoteJorge>();
+
+    public AllNotes() =>
+        LoadNotes();
+
+    public void LoadNotes()
+    {
+        Notes.Clear();
+
+        // Get the folder where the notes are stored.
+        string appDataPath = FileSystem.AppDataDirectory;
+
+        // Use Linq extensions to load the *.notes.txt files.
+        IEnumerable<NoteJorge> notes = Directory
+
+                                    // Select the file names from the directory
+                                    .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                                    // Each file name is used to create a new Note
+                                    .Select(filename => new NoteJorge()
+                                    {
+                                        Filename = filename,
+                                        Text = File.ReadAllText(filename),
+                                        Date = File.GetCreationTime(filename)
+                                    })
+
+                                    // With the final collection of notes, order them by date
+                                    .OrderBy(note => note.Date);
+
+        // Add each note into the ObservableCollection
+        foreach (NoteJorge note in notes)
+            Notes.Add(note);
+    }
+}
